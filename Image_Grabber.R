a6e91db10c138e27d6448ad5a3c064b07bc1152a
Winston Saunders <winstonasaunders@gmail.com>
2015-11-11 22:05:48 -0800

    update frequencies

--- Image_Grabber.R
+++ Image_Grabber.R
@@ -9,9 +9,13 @@ setwd("/Users/winstonsaunders/Documents/")
 setwd(paste0(getwd(), "/Road_Images"))
 image_save_dir<-paste0(getwd(), "/Image_Data")
 
+## number of days to record images
 day_record<-3
-for (i in 1:day_record*24*2){
-    Sys.sleep(60*30/100)
+## period in minutes between image grabs
+minutes<-30
+
+for (i in 1:day_record*24*60/minutes){
+    Sys.sleep(60*minutes)
     cat(paste("Read: ", as.POSIXlt(Sys.time()), "\n"))
     
     ## download file
