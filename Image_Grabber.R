79a3f8479c20c9cf308349bef4c90e15f6614325
Winston Saunders <winstonasaunders@gmail.com>
2015-11-16 16:18:47 -0800

    some updated

--- Image_Grabber.R
+++ Image_Grabber.R
@@ -1,7 +1,7 @@
-## download Periodic Data from n
+## download Periodic Data from a website
 ##
-## this is a machine learning project
-## to automatically analyze road conditions
+## this is part of a machine learning project
+## to analyze road conditions automatically
 ##
 
 setwd("/Users/winstonsaunders/Documents/")
@@ -10,9 +10,13 @@ setwd(paste0(getwd(), "/Road_Images"))
 image_save_dir<-paste0(getwd(), "/Image_Data")
 
 ## number of days to record images
-day_record<-3
+day_record<-2
 ## period in minutes between image grabs
-minutes<-30
+minutes<-10
+
+number_of_files<-day_record*24*(60/minutes)
+
+cat(paste("The program will download", as.character(number_of_files ) , "files over", day_record,"days \n"))
 
 for (i in 1:day_record*24*60/minutes){
     Sys.sleep(60*minutes)
@@ -20,9 +24,9 @@ for (i in 1:day_record*24*60/minutes){
     
     ## download file
     download.file("http://tripcheck.com/RoadCams/cams/Santiam%20Pass_pid2728.JPG?rand=567",
-                  destfile=paste0(image_save_dir,"/",Sys.time(), "santiam.jpg"),mode = 'wb')
+                  destfile=paste0(image_save_dir,"/",Sys.time(), "_santiam.jpg"),mode = 'wb')
     
 }
 
-cat("End:  ",Sys.time(), "\n")
+cat(paste("End:  ",as.POSIXlt(Sys.time()), "\n"))
 
